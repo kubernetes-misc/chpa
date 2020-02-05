e97181fb0c43e43874f12f7a4f01594cada8c529
Captain Justin <just1689@gmail.com>
2020-02-05 13:48:17 +0200

    cronscales model

---
+++ model/cron-scales.go
@@ -0,0 +1,33 @@
+package model
+
+import "k8s.io/apimachinery/pkg/runtime/schema"
+
+var CronScaleV1CRDSchema = schema.GroupVersionResource{
+	Group:    "captainjustin.space",
+	Version:  "v1",
+	Resource: "cronscales",
+}
+
+type CronScaleV1 struct {
+	Metadata MetadataV1 `json:"metadata"`
+	Spec     SpecV1     `json:"spec"`
+}
+
+type MetadataV1 struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+}
+
+type SpecV1 struct {
+	CronSpec                       string           `json:"cronSpec"`
+	MaxReplicas                    int              `json:"maxReplicas"`
+	MinReplicas                    int              `json:"minReplicas"`
+	TargetCPUUtilizationPercentage int              `json:"targetCPUUtilizationPercentage"`
+	ScaleTargetRef                 ScaleTargetRefV1 `json:"scaleTargetRef"`
+}
+
+type ScaleTargetRefV1 struct {
+	ApiVersion string `json:"apiVersion"`
+	Kind       string `json:"kind"`
+	Name       string `json:"name"`
+}
